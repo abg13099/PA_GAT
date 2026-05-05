af916e01a4d4795809b51a96b6d0ffd988fa20c4
Ahmed Guler <abg13099@uga.edu>
2026-05-05 12:36:07 -0400

    Results for variance test on node classification datasets.

--- run_variance_experiment.zsh
+++ run_variance_experiment.zsh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 #
 # Runs variance study on the target datasets
 #
 
-python3 variance_study.py ENZYMES 25
-python3 variance_study.py PROTEINS 25
-python3 variance_study.py COLLAB 25
-python3 variance_study.py REDDIT-BINARY 25
-python3 variance_study.py DD 25
-python3 variance_study.py NCI1 25
+python3 variance_study.py Cora 50
+python3 variance_study.py Citeseer 50
+python3 variance_study.py Pubmed 50
+python3 variance_study.py Texas 50
+python3 variance_study.py Wisconsin 50
+python3 variance_study.py Cornell 50
+python3 variance_study.py Physics 50
+python3 variance_study.py CS 50
